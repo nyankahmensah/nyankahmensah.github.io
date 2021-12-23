7aea68d0ec1bd0a5faca0923709e8586d4c62d94
Nyankah Mensah <nyankahmensah@gmail.com>
2021-12-23 08:29:54 +0000

    Updates

---
+++ precache-manifest.767aaf7c8d5c893638b633bc40a8b265.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8ad61019e58633760ed83f8e8dc268",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33d42613e73ec8f0e4d8",
+    "url": "/static/css/main.c9ba0546.chunk.css"
+  },
+  {
+    "revision": "3b2392e70f65e5f924be",
+    "url": "/static/js/2.32987689.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.32987689.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33d42613e73ec8f0e4d8",
+    "url": "/static/js/main.e611fcc3.chunk.js"
+  },
+  {
+    "revision": "9d892c65bfdc7fb83e1a",
+    "url": "/static/js/runtime-main.d1d5d272.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
